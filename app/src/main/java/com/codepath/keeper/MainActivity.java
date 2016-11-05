0d1b4fc4a3be9945ee56da7fe18e40c5acb3e268
Michael Sova <michael.sova@datafox.com>
2016-11-05 15:48:20 -0700

    First commit

---
+++ app/src/main/java/com/codepath/keeper/MainActivity.java
@@ -0,0 +1,13 @@
+package com.codepath.keeper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //something
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
