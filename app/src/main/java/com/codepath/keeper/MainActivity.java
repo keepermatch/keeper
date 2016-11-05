ba36b97922479e0f6c7c2b60921eb158d62bbedc
Michael Sova <michael.sova@datafox.com>
2016-11-05 16:11:50 -0700

    added crashanalytics

--- app/src/main/java/com/codepath/keeper/MainActivity.java
+++ app/src/main/java/com/codepath/keeper/MainActivity.java
@@ -1,13 +1,18 @@
 package com.codepath.keeper;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
-    //something
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
     }
 }
