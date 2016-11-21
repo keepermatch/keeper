4204a9c4ed55747f99d7f68e18251451035b4a21
Michael Sova <michael.sova@datafox.com>
2016-11-20 18:05:55 -0800

    ok , contact providers done

--- app/src/main/java/com/codepath/keeper/activities/VouchForAFriend.java
+++ app/src/main/java/com/codepath/keeper/activities/VouchForAFriend.java
@@ -1,11 +1,11 @@
 package com.codepath.keeper.activities;
 
-import android.app.LoaderManager;
-import android.content.CursorLoader;
-import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -31,10 +31,44 @@ public class VouchForAFriend extends AppCompatActivity {
     ArrayList<User> userFriendsArrayList;
     UserFriendsAdapter userFriendsAdapter;
 
-    ArrayList<User> userContactsArrayList;
     public static final int CONTACT_LOADER_ID = 78; // From docs: A unique identifier for this loader. Can be whatever you want.
 
     private SimpleCursorAdapter cursorAdapter;
+
+    // Defines the asynchronous callback for the contacts data loader
+    private LoaderManager.LoaderCallbacks<Cursor> contactsLoader =
+            new LoaderManager.LoaderCallbacks<Cursor>() {
+                // Create and return the actual cursor loader for the contacts data
+                @Override
+                public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+                    // Define the columns to retrieve
+                    String[] projectionFields = new String[] { ContactsContract.Contacts._ID,
+                            ContactsContract.Contacts.DISPLAY_NAME,
+                            ContactsC   ontract.Contacts.PHOTO_URI };
+                    // Construct the loader
+                    CursorLoader cursorLoader = new CursorLoader(VouchForAFriend.this,
+                            ContactsContract.Contacts.CONTENT_URI, // URI
+                            projectionFields, // projection fields
+                            null, // the selection criteria
+                            null, // the selection args
+                            null // the sort order
+                    );
+                    // Return the loader for use
+                    return cursorLoader;
+                }
+
+
+                @Override
+                public void onLoadFinished(android.support.v4.content.Loader<Cursor> loader, Cursor data) {
+                    cursorAdapter.swapCursor(data);
+
+                }
+
+                @Override
+                public void onLoaderReset(android.support.v4.content.Loader<Cursor> loader) {
+                    cursorAdapter.swapCursor(null);
+                }
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,8 +77,7 @@ public class VouchForAFriend extends AppCompatActivity {
         setupCursorAdapter();
 
         // Initialize the loader with a special ID and the defined callbacks from above
-        getSupportLoaderManager().initLoader(CONTACT_LOADER_ID,
-                new Bundle(), (android.support.v4.app.LoaderManager.LoaderCallbacks<Object>) contactsLoader);
+        getSupportLoaderManager().initLoader(CONTACT_LOADER_ID, new Bundle(), contactsLoader);
 
 
         userFriendsArrayList = new ArrayList<>();
@@ -100,43 +133,4 @@ public class VouchForAFriend extends AppCompatActivity {
                 0);
     }
 
-    // Defines the asynchronous callback for the contacts data loader
-    private LoaderManager.LoaderCallbacks<Cursor> contactsLoader =
-            new LoaderManager.LoaderCallbacks<Cursor>() {
-                // Create and return the actual cursor loader for the contacts data
-                @Override
-                public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-                    // Define the columns to retrieve
-                    String[] projectionFields = new String[] { ContactsContract.Contacts._ID,
-                            ContactsContract.Contacts.DISPLAY_NAME,
-                            ContactsContract.Contacts.PHOTO_URI };
-                    // Construct the loader
-                    CursorLoader cursorLoader = new CursorLoader(VouchForAFriend.this,
-                            ContactsContract.Contacts.CONTENT_URI, // URI
-                            projectionFields, // projection fields
-                            null, // the selection criteria
-                            null, // the selection args
-                            null // the sort order
-                    );
-                    // Return the loader for use
-                    return cursorLoader;
-                }
-
-                // When the system finishes retrieving the Cursor through the CursorLoader,
-                // a call to the onLoadFinished() method takes place.
-                @Override
-                public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-                    // The swapCursor() method assigns the new Cursor to the adapter
-                    cursorAdapter.swapCursor(cursor);
-                }
-
-                // This method is triggered when the loader is being reset
-                // and the loader data is no longer available. Called if the data
-                // in the provider changes and the Cursor becomes stale.
-                @Override
-                public void onLoaderReset(Loader<Cursor> loader) {
-                    // Clear the Cursor we were using with another call to the swapCursor()
-                    cursorAdapter.swapCursor(null);
-                }
-            };
 }
